3c4e990710b022a7593b362579f24e1a436f4e94
cnobi <shr.susan7@gmail.com>
2022-07-22 21:44:28 +0545

    Fixed

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post("/", function(req, res){
         })
     })
 
-    // request.write(jsonData);
+    request.write(jsonData);
     request.end();
 
     
@@ -66,7 +66,7 @@ app.post("/failure", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started at Port 3000 Sucessfully.");
 });
 
